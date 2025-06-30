31e12a40e2e0aea4e0e65a26e2c74a743fc0f11e
Kalpesh <kalpeshlokhande0355@gmail.com>
2025-06-30 12:11:09 +0530

    sigup views

--- spotify_clone/music/urls.py
+++ spotify_clone/music/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name="index"),
+    path('login',views.login,name="login"),
+    path('signup',views.signup,name="signup"),
+    path('logout',views.index,name="logout"),
 
 ]
